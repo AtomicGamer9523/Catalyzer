b130a2fd7a924694b4f588236d2438f7e78ddcbd
AtomicGamer9523 <drunkenmonkey4987@gmail.com>
2024-03-17 20:24:02 -0700

    this too...

--- catalyzer.rs
+++ catalyzer.rs
@@ -1,7 +1,7 @@
 #![crate_name = "catalyzer"]
-#![doc(html_favicon_url = "https://raw.githubusercontent.com/AtomicGamer9523/Catalyzer/main/doc/logo.ico")]
-#![doc(html_logo_url = "https://raw.githubusercontent.com/AtomicGamer9523/Catalyzer/main/doc/logo.png")]
-#![doc(html_root_url = "https://gh.matveit.dev/Catalyzer/catalyzer/")]
+#![doc(html_favicon_url = "https://raw.githubusercontent.com/AtomicGamer9523/Catalyzer/main/.github/doc/logo.ico")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/AtomicGamer9523/Catalyzer/main/.github/doc/logo.png")]
+#![doc(html_root_url = "https://gh.matveit.dev/Catalyzer/")]
 #![doc(html_no_source)]
 
 //! Catalyzer is a web framework for Rust, made to be simple and easy to use.
